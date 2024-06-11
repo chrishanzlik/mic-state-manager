783d3560c37cbdc0168d40656ccd5e7748d3b08b
chrishanzlik <senz2702@gmail.com>
2024-06-11 18:02:35 +0200

    Check idle; MicState enum

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,32 @@
 #define RED_LED 3
 #define BLUE_LED 4
 
+enum MicState {
+  Unknown = 0,
+  Muted = 1,
+  Activated = 2
+};
+
 void toggleMicrophone();
 void processSerialData();
 bool buttonPressed();
+void checkIdle();
 
-int switchState;
-int previousState = LOW;
-bool mic_muted = false;
-unsigned long lastToggled = 0;
-unsigned long debounceTime = 500UL;
+int switch_state;
+int previous_switch_state = LOW;
+MicState mic_state = MicState::Unknown;
+unsigned long last_toggled_ms = 0;
+unsigned long debounce_time_ms = 500UL;
+unsigned long last_received_ms = 0;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(SWITCH, INPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
+
+  _delay_ms(500);
+
+  Serial.begin(9600);
 
   digitalWrite(RED_LED, LOW);
   digitalWrite(BLUE_LED, LOW);
@@ -26,36 +37,65 @@ void setup() {
 
 void loop() {
   processSerialData();
-  switchState = digitalRead(SWITCH);
+  switch_state = digitalRead(SWITCH);
   
   if (buttonPressed()) {
     toggleMicrophone();
-    lastToggled = millis();
+    last_toggled_ms = millis();
   }
 
-  previousState = switchState;
+  previous_switch_state = switch_state;
+
+  checkIdle();
 }
 
 bool buttonPressed() {
-  return switchState == HIGH
-    && previousState == LOW
-    && millis() - lastToggled > debounceTime;
+  return switch_state == HIGH
+    && previous_switch_state == LOW
+    && millis() - last_toggled_ms > debounce_time_ms;
 }
 
+void checkIdle() {
+  bool isIdle = last_received_ms == 0 || millis() - last_received_ms > 1000 * 10;
+
+  if (isIdle) {
+    _delay_ms(200);
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(BLUE_LED, HIGH);
+    _delay_ms(1500);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(BLUE_LED, LOW);
+  }
+}
 
 void toggleMicrophone() {
-  int data = mic_muted ? 1 : 0;
+  int data = mic_state == MicState::Activated ? 1 : 0;
   Serial.write(data);
   Serial.flush();
 }
 
 void processSerialData() {
   if (Serial.available() > 0) {
+
+    last_received_ms = millis();
+
     while(Serial.available() > 0) {
-      mic_muted = Serial.read() == 0;
+      int data = Serial.read();
+
+      if (data == 6)
+      {
+        _delay_ms(100);
+        digitalWrite(RED_LED, HIGH);
+        digitalWrite(BLUE_LED, HIGH);
+        Serial.write(6);
+        Serial.flush();
+        return;
+      }
+
+      mic_state = data == 0 ? MicState::Muted : MicState::Activated;
     }
 
-    digitalWrite(mic_muted ? RED_LED : BLUE_LED, HIGH);
-    digitalWrite(mic_muted ? BLUE_LED : RED_LED, LOW);
+    digitalWrite(mic_state == MicState::Muted ? RED_LED : BLUE_LED, HIGH);
+    digitalWrite(mic_state == MicState::Muted ? BLUE_LED : RED_LED, LOW);
   }
 }
